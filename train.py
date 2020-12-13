244ed82e48edf37c504e980301499b9fd1c19af7
Sam Ehrenstein <sam.ehrenstein@gmail.com>
2020-12-13 17:57:30 -0500

    added some rat data

--- train.py
+++ train.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import scale
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
 
 def load_data(fname):
     df = pd.read_excel(fname)
@@ -15,9 +16,12 @@ def load_data(fname):
 
 def train_classifier(df):
     data = df[['D0A0', 'D0A6 and D2A0','D0S0', 'D0S6 and D2S0', 'D2A6', 'D2S6', 'D0A6']]
+    # data = df[['D0A0', 'D0A6 and D2A0', 'D0S6 and D2S0', 'D2S6', 'D0A6']]
     data = data.to_numpy()
     labels = df['Y1A2PD3']
     labels=labels.to_numpy(dtype=int)
+    # labels=np.maximum(labels,2)
+    # labels=labels%2
 
     X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.3, random_state=5)
 
@@ -31,12 +35,17 @@ def train_classifier(df):
     # plt.show()
     # print(data)
     # print(labels)
-    clf = svm.SVC(kernel='poly')
+    
+    # clf = svm.SVC(kernel='poly')
+    clf = MLPClassifier(hidden_layer_sizes=(20,20,20), max_iter=1000, learning_rate_init=1e-2, activation='logistic')
     clf.fit(X_train, y_train)
+    # rs: 1,2
 
     # X_test = pca.transform(X_test)
     X_test = scale(X_test)
     preds = clf.predict(X_test)
+    prob_preds = clf.predict_proba(X_test)
+    print(prob_preds)
     print(preds)
     print(y_test)
     print(confusion_matrix(y_test, preds))
